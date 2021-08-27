3b41a755f735fd461e2961b0bbced3920c3ebe6d
anil <anil.kumar.ait09@gmail.com>
2021-08-28 01:55:48 +1000

    API - implemented functionality

---
+++ src/main/java/com/squirrel/persons/controller/WantedPersonController.java
@@ -0,0 +1,27 @@
+package com.squirrel.persons.controller;
+
+import com.squirrel.persons.service.WantedPersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController("/wanted")
+public class WantedPersonController {
+
+private  WantedPersonService wantedPersonService;
+    @Autowired
+    public WantedPersonController(WantedPersonService wantedPersonService) {
+        this.wantedPersonService = wantedPersonService;
+    }
+
+    @PostMapping("/refresh")
+    public void refreshWantedPersons() throws IOException {
+        wantedPersonService.refreshWantedPersons();
+    }
+
+
+
+
+}
---
+++ src/main/java/com/squirrel/persons/service/WantedPersonService.java
@@ -0,0 +1,69 @@
+package com.squirrel.persons.service;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Set;
+
+@Service
+public class WantedPersonService {
+    public static final String CRIME_STOPPERS_VIC_WANTED_PERSONS_URL = "https://www.crimestoppersvic.com.au/help-solve-crime/wanted-persons/";
+
+    public void refreshWantedPersons() throws IOException {
+        Set<String> picturesUrl = getAllPictures();
+        Iterator<String> it = picturesUrl.iterator();
+        while (it.hasNext()) {
+            String url = it.next();
+            URLConnection connection = new URL(url).openConnection();
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+            connection.connect();
+            try (InputStream in = connection.getInputStream()) {
+                Files.copy(in, Paths.get("/usr/local/squirrel-ai/wanted-criminals/" + new Random().nextInt() + ".png"), StandardCopyOption.REPLACE_EXISTING);
+            }
+        }
+    }
+
+    private Set<String> getAllPictures() throws IOException {
+        Set<String> imagesUrl = new HashSet<>();
+        for (int page = 1; page < 10; page++) {
+            Document document = Jsoup.connect(CRIME_STOPPERS_VIC_WANTED_PERSONS_URL + page).get();
+            Elements elements = document.getElementsByClass("type-wanted_persons");  //wanted_persons
+
+            for (int i = 0; i < elements.size(); i++) {
+                Element eachElement = elements.get(i);
+                Elements eachElementSub = eachElement.getElementsByClass("elementor-section-wrap");
+                for (int j = 0; j < eachElementSub.size(); j++) {
+                    String data = eachElementSub.get(j).data();
+                    int startIndex = data.indexOf("https://www.crimestoppersvic.com.au");
+                    int endIndexPNG = data.indexOf(".png");
+                    int endIndexJPG = data.indexOf(".jpg");
+                    int endIndexJPEG = data.indexOf(".jpeg");
+                    String extractedUrl;
+                    if (endIndexPNG != -1) {
+                        extractedUrl = data.substring(startIndex, endIndexPNG) + ".png";
+                    } else if (endIndexJPG != -1) {
+                        extractedUrl = data.substring(startIndex, endIndexJPG) + ".jpg";
+                    } else {
+                        extractedUrl = data.substring(startIndex, endIndexJPEG) + ".jpeg";
+                    }
+
+                    imagesUrl.add(extractedUrl);
+                }
+            }
+        }
+        return imagesUrl;
+    }
+}
