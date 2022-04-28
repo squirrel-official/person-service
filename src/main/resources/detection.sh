1a3953a525cf27577c61022e6deca9a2ee8b84d1
anil <anil.kumar.ait09@gmail.com>
2022-04-28 18:52:25 +1000

    API - Added code to truncate old logs

--- src/main/resources/detection.sh
+++ src/main/resources/detection.sh
@@ -1,2 +1,3 @@
+echo "" > /usr/local/squirrel-ai/logs/service.log
 sudo -u pi /usr/bin/python3  /usr/local/squirrel-ai/service/motionDetection.py
 # touch /usr/local/squirrel-ai/end.txt  -- Inorder to test whether the process executed successfully or not.
