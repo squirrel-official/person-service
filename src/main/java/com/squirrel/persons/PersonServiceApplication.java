477c823f1e5837645ebfd0eca2637180a8a74e14
anil <anil.kumar.ait09@gmail.com>
2022-05-26 16:29:31 +1000

    API - Added code for intelligent detection at start

--- src/main/java/com/squirrel/persons/PersonServiceApplication.java
+++ src/main/java/com/squirrel/persons/PersonServiceApplication.java
@@ -1,13 +1,27 @@
 package com.squirrel.persons;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class PersonServiceApplication {
+    private static final Logger LOGGER = LogManager.getLogger(PersonServiceApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(PersonServiceApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner CommandLineRunnerBean() {
+        return (args) -> {
+            Process process = Runtime.getRuntime().exec("sh /usr/local/person-service/src/main/resources/detection.sh");
+            LOGGER.debug("detection process started {}", process);
+        };
     }
 }
