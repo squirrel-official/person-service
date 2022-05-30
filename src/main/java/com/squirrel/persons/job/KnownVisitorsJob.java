a17336768d1a3119d45fd0f81e96f7754882a256
anil <anil.kumar.ait09@gmail.com>
2022-05-30 21:49:22 +1000

    API - Added file

---
+++ src/main/java/com/squirrel/persons/job/KnownVisitorsJob.java
@@ -0,0 +1,41 @@
+package com.squirrel.persons.job;
+
+import com.itextpdf.text.DocumentException;
+import com.squirrel.persons.service.EmailService;
+import com.squirrel.persons.util.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+
+@Component
+public class KnownVisitorsJob {
+    public static final String VISITOR_PATH = "/usr/local/squirrel-ai/result/known-visitors/";
+    public static final String VISITOR_ARCHIVE_PATH = "/usr/local/squirrel-ai/data/archives/known-visitors/";
+    @Value("${mail.recipient}")
+    private String toEmailAddress;
+
+    public final EmailService emailService;
+
+    private static final Logger LOGGER = LogManager.getLogger(KnownVisitorsJob.class);
+
+    @Autowired
+    public KnownVisitorsJob(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @Scheduled(fixedDelay = 3600000)
+    public void triggerJob() throws MessagingException, DocumentException, IOException {
+        LOGGER.info("Triggering known visitor job");
+        emailService.attachImagesAndSendEmail(toEmailAddress, VISITOR_PATH,"Known visitors",
+                "Somebody you know is on gate");
+        FileUtils.copyAllFiles(VISITOR_PATH, VISITOR_ARCHIVE_PATH);
+//        FileUtils.deleteImages(VISITOR_ARCHIVE_PATH);
+    }
+
+}
