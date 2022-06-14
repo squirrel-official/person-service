1daf1312f089e8945a515c60ae9d5af6d9f46d81
anil <anil.kumar.ait09@gmail.com>
2022-06-14 16:32:07 +1000

    API - motion health check

--- src/main/java/com/squirrel/persons/job/MotionHealthCheckJob.java
+++ src/main/java/com/squirrel/persons/job/MotionHealthCheckJob.java
@@ -7,11 +7,11 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+
 @Component
 public class MotionHealthCheckJob {
-
-    String url = "http://localhost::7777";
-    private static final Logger LOGGER = LogManager.getLogger(MotionHealthCheckJob.class);
+    String url = "http://localhost:7777/";
+    private static final Logger LOGGER = LogManager.getLogger(com.squirrel.persons.job.MotionHealthCheckJob.class);
     private RestTemplate restTemplate;
 
     @Autowired
@@ -19,15 +19,28 @@ public class MotionHealthCheckJob {
         this.restTemplate = restTemplate;
     }
 
-    @Scheduled(fixedDelay = 300000)
+    @Scheduled(fixedDelay = 30000)
     public void triggerJob() {
-        try {
-            LOGGER.info("Motion health check job Start");
-            String result = restTemplate.getForObject(url, String.class);
-            LOGGER.info("Motion health check job End {}", result);
-        }catch (Exception ex){
-            LOGGER.error(ex);
-        }
+       if(! isMotionServiceUp()){
+           try {
+               Runtime.getRuntime().exec("sudo motion restart");
+               LOGGER.error("Motion software is being restarted");
+           }catch (Exception e){
+               LOGGER.error("An error happened",e);
+           }
+       }
     }
 
+    private boolean isMotionServiceUp() {
+        try {
+            LOGGER.debug("Motion health check job Start");
+            restTemplate.getForObject(url, String.class);
+            LOGGER.debug("Motion health check job complete");
+            return  Boolean.TRUE;
+        }catch (Exception ex){
+            LOGGER.error("The Url {} is not available, scheduling restart of system", url);
+            return  Boolean.TRUE;
+
+        }
+    }
 }
