d697f899687903fab9f2aff0d046e1eb59f7aaa6
anil <anil.kumar.ait09@gmail.com>
2022-06-15 23:38:42 +1000

    API - motion health check

--- src/main/java/com/squirrel/persons/job/MotionHealthCheckJob.java
+++ src/main/java/com/squirrel/persons/job/MotionHealthCheckJob.java
@@ -7,44 +7,62 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.*;
 
 @Component
 public class MotionHealthCheckJob {
     String preUrl = "http://my-security.local:7776/";
+    String motionRestartCommand = "/usr/local/person-service/src/main/resources/motion-restart.sh";
     private static final Logger LOGGER = LogManager.getLogger(com.squirrel.persons.job.MotionHealthCheckJob.class);
     private RestTemplate restTemplate;
 
     @Autowired
-    public MotionHealthCheckJob(RestTemplate restTemplate){
+    public MotionHealthCheckJob(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     @Scheduled(fixedDelay = 30000)
     public void triggerJob() {
-       if(! isMotionServiceUp()){
-           try {
-               Runtime.getRuntime().exec("sudo motion restart");
-               LOGGER.error("Motion software is being restarted");
-           }catch (Exception e){
-               LOGGER.error("An error happened",e);
-           }
-       }
+        if (!isMotionServiceUp()) {
+            try {
+                Runtime.getRuntime().exec(motionRestartCommand);
+                LOGGER.error("Motion software is being restarted");
+            } catch (Exception e) {
+                LOGGER.error("An error happened", e);
+            }
+        }
     }
 
     private boolean isMotionServiceUp() {
-        for(int i=1; i<4; i++) {
+        LOGGER.debug("Motion health check job Start");
+        boolean result = true;
+        for (int i = 1; i < 4; i++) {
             try {
-                String url=preUrl+i+"/stream/";
-                LOGGER.debug("Motion health check job Start");
-                restTemplate.getForObject(url, String.class);
-                LOGGER.debug("Motion health check job complete");
-                return Boolean.TRUE;
+                String url = preUrl + i + "/stream/";
+                boolean reachable = isServerReachable(url);
+                LOGGER.debug(reachable);
             } catch (Exception ex) {
                 LOGGER.error("The stream id {} is not available, scheduling restart of motion system", i, ex);
-                return Boolean.FALSE;
-
+                return false;
             }
         }
-        return Boolean.TRUE;
+        return result;
+    }
+
+    public boolean isServerReachable(String url) {
+        try {
+            URL urlServer = new URL(url);
+            HttpURLConnection urlConn = (HttpURLConnection) urlServer.openConnection();
+            urlConn.setConnectTimeout(5000);
+            urlConn.connect();
+            if (urlConn.getResponseCode() == 200) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            LOGGER.error(e);
+        }
+        return false;
     }
 }
--- src/main/resources/motion-restart.sh
+++ src/main/resources/motion-restart.sh
@@ -1,3 +1,2 @@
-echo "" > /var/log/motion/motion.log
 sudo -u pi /usr/bin/motion restart
- touch /usr/local/squirrel-ai/end.txt
+touch /usr/local/person-service/end.txt
