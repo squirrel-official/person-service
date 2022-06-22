c34a8c3bfc91d854bc9de7896609bedc35b49cbc
anil <anil.kumar.ait09@gmail.com>
2022-06-22 21:52:40 +1000

    API - Code refactoring for fast notifications

--- src/main/java/com/squirrel/persons/aspect/ExecutionTimeAdvice.java
+++ src/main/java/com/squirrel/persons/aspect/ExecutionTimeAdvice.java
@@ -1,6 +1,9 @@
 package com.squirrel.persons.aspect;
 
+import com.squirrel.persons.service.EmailService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -10,12 +13,14 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class ExecutionTimeAdvice {
+    private static final Logger LOGGER = LogManager.getLogger(ExecutionTimeAdvice.class);
+
     @Around("@annotation(com.squirrel.persons.aspect.TrackExecutionTime)")
     public Object executionTime(ProceedingJoinPoint point) throws Throwable {
         long startTime = System.currentTimeMillis();
         Object object = point.proceed();
         long endTime = System.currentTimeMillis();
-        log.debug("Class Name: "+ point.getSignature().getDeclaringTypeName() +". Method Name: "+ point.getSignature().getName() + ". Time taken for Execution is : " + (endTime-startTime) +"ms");
+        LOGGER.debug("Class Name: "+ point.getSignature().getDeclaringTypeName() +". Method Name: "+ point.getSignature().getName() + ". Time taken for Execution is : " + (endTime-startTime) +"ms");
         return object;
     }
 }
