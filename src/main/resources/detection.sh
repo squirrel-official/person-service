66bb0702727ea41669fdbb548e182bf0c00a58fa
anil <anil.kumar.ait09@gmail.com>
2022-07-04 22:46:14 +1000

    API - fix for emails

--- src/main/resources/detection.sh
+++ src/main/resources/detection.sh
@@ -1,5 +1,5 @@
 # This will clear all logs and then restart service
 echo "" > /usr/local/squirrel-ai/logs/service.log
+echo "" > /usr/local/squirrel-ai/logs/motion.log
 echo "" > /usr/local/person-service/logs/application.log
-echo "" > /var/log/motion/motion.log
 sudo -u pi /usr/bin/python3  /usr/local/squirrel-ai/service/videoDetection.py
