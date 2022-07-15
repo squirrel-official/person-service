c2f36b83e6ada13e509ecd6266bfeb3d80642d6f
anil <anil.kumar.ait09@gmail.com>
2022-07-16 00:40:47 +1000

    API - Modified the file to run detection as server

--- src/main/resources/detection.sh
+++ src/main/resources/detection.sh
@@ -3,6 +3,8 @@ echo "" > /usr/local/squirrel-ai/logs/service.log
 echo "" > /usr/local/squirrel-ai/logs/motion.log
 echo "" > /usr/local/squirrel-ai/logs/detection.log
 echo "" > /usr/local/squirrel-ai/logs/notification.log
+
 export FLASK_APP=/usr/local/squirrel-ai/service/web.py
-nohup flask run &
+sudo -u pi /usr/bin/python3  /usr/local/squirrel-ai/service/web.py
+
 #sudo -u pi /usr/bin/python3  /usr/local/squirrel-ai/service/videoDetection.py
