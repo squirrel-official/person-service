1da97e32cf8c4c6745a3b55916f0e6a1d3077e6f
anil <anil.kumar.ait09@gmail.com>
2022-07-26 00:00:41 +1000

    API - Updated controller

--- src/main/resources/detection.sh
+++ src/main/resources/detection.sh
@@ -4,7 +4,7 @@ echo "" > /usr/local/squirrel-ai/logs/motion.log
 echo "" > /usr/local/squirrel-ai/logs/detection.log
 echo "" > /usr/local/squirrel-ai/logs/notification.log
 
-export FLASK_APP=/usr/local/squirrel-ai/service/web.py
-sudo -u pi /usr/bin/python3  /usr/local/squirrel-ai/service/web.py
-
+#export FLASK_APP=/usr/local/squirrel-ai/service/web.py
+#sudo -u pi /usr/bin/python3  /usr/local/squirrel-ai/service/web.py
+sudo -u pi /usr/bin/python3  /usr/local/squirrel-ai/service/motionDetection.py
 #sudo -u pi /usr/bin/python3  /usr/local/squirrel-ai/service/videoDetection.py
