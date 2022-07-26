556523fb8d9f74ad90588c46ea5c6c2f79f0b266
anil <anil.kumar.ait09@gmail.com>
2022-07-26 16:10:08 +1000

    API - Updated controller

--- src/main/java/com/squirrel/persons/controller/NotificationsController.java
+++ src/main/java/com/squirrel/persons/controller/NotificationsController.java
@@ -74,7 +74,7 @@ public class NotificationsController {
 
     @PostMapping("/attach-notify")
     public void sendNotificationWithImage(@RequestParam("camera-id") String cameraId,
-                                          @RequestParam("file") MultipartFile multipartFile) {
+                                          @RequestParam("image") MultipartFile multipartFile) {
         if (isCoolDownExpired()) {
             String cameraName = cameraId != null ? cameraId : "General Camera";
             LOGGER.info("received notification from camera : {}", cameraName);
