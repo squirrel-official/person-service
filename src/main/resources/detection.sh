84feff28078a7273f36fd5176a4c24a62aabbcea
anil <anil.kumar.ait09@gmail.com>
2022-07-29 01:24:36 +1000

    API - fixing notifications

--- src/main/resources/detection.sh
+++ src/main/resources/detection.sh
@@ -3,7 +3,7 @@ echo "" > /usr/local/squirrel-ai/logs/service.log
 echo "" > /usr/local/squirrel-ai/logs/motion.log
 echo "" > /usr/local/squirrel-ai/logs/detection.log
 echo "" > /usr/local/squirrel-ai/logs/notification.log
-
+sudo modprobe v4l2loopback devices=2
 #export FLASK_APP=/usr/local/squirrel-ai/service/web.py
 #sudo -u pi /usr/bin/python3  /usr/local/squirrel-ai/service/web.py
 sudo -u pi /usr/bin/python3  /usr/local/squirrel-ai/service/motionDetection.py
