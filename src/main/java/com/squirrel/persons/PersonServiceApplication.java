9aea52902d59715ef343425c82562d3cd749b322
anil <anil.kumar.ait09@gmail.com>
2022-08-01 13:57:09 +1000

    API - fixing notifications

--- src/main/java/com/squirrel/persons/PersonServiceApplication.java
+++ src/main/java/com/squirrel/persons/PersonServiceApplication.java
@@ -3,10 +3,12 @@ package com.squirrel.persons;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAspectJAutoProxy
 public class PersonServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(PersonServiceApplication.class, args);
--- src/main/java/com/squirrel/persons/aspect/ExecutionTimeAdvice.java
+++ src/main/java/com/squirrel/persons/aspect/ExecutionTimeAdvice.java
@@ -1,6 +1,5 @@
 package com.squirrel.persons.aspect;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
