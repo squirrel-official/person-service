52442cd5b3740820f13c6e2e37bd86b5a1443a9f
anil <anil.kumar.ait09@gmail.com>
2022-08-25 23:53:31 +1000

    API - AI-Security related change to remove motion dependency

--- src/main/java/com/squirrel/persons/job/MotionHealthCheckJob.java
+++ src/main/java/com/squirrel/persons/job/MotionHealthCheckJob.java
@@ -6,8 +6,6 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -23,17 +21,6 @@ public class MotionHealthCheckJob {
 
     @Scheduled(fixedDelay = 180000)
     public void triggerJob() {
-        if (!isMotionServiceUp()) {
-            try {
-                LOGGER.info("Motion software is being restarted");
-                Runtime.getRuntime().exec(motionRestartCommand);
-                Process process = Runtime.getRuntime().exec(motionRestartCommand);
-                LOGGER.info("Motion software restarted {}", process);
-                Uninterruptibles.sleepUninterruptibly(1, TimeUnit.MINUTES);
-            } catch (Exception e) {
-                LOGGER.error("An error happened", e);
-            }
-        }
         if (isAIServiceDown()) {
             try {
                 Process detectionProcess = Runtime.getRuntime().exec("sh /usr/local/person-service/src/main/resources/detection.sh");
@@ -43,16 +30,6 @@ public class MotionHealthCheckJob {
                 LOGGER.error("An error happened", e);
             }
         }
-    }
-
-    private boolean isMotionServiceUp() {
-        for (int i = 1; i < 2; i++) {
-            String url = preUrl + i + "/stream/";
-            if (!isServerReachable(url)) {
-                return false;
-            }
-        }
-        return true;
     }
 
     private  boolean isAIServiceDown(){
@@ -72,22 +49,5 @@ public class MotionHealthCheckJob {
         }else {
             return new String[0];
         }
-    }
-
-    public boolean isServerReachable(String url) {
-        try {
-            URL urlServer = new URL(url);
-            HttpURLConnection urlConn = (HttpURLConnection) urlServer.openConnection();
-            urlConn.setConnectTimeout(5000);
-            urlConn.connect();
-            if (urlConn.getResponseCode() == 200) {
-                return true;
-            } else {
-                LOGGER.error("The  url {} is not available", url);
-            }
-        } catch (Exception e) {
-            LOGGER.error("The  url {} is not available", url,  e);
-        }
-        return false;
     }
 }
