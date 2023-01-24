0531c90db53186cbb70f4136b45e69db3a02a133
anil <anil.kumar.ait09@gmail.com>
2023-01-24 23:29:47 +1100

    commented the logs file truncation

--- src/main/resources/detection.sh
+++ src/main/resources/detection.sh
@@ -1,10 +1,7 @@
 # This will clear all logs and then restart service
-echo "" > /usr/local/squirrel-ai/logs/service.log
-echo "" > /usr/local/squirrel-ai/logs/motion.log
-echo "" > /usr/local/squirrel-ai/logs/detection.log
-echo "" > /usr/local/squirrel-ai/logs/notification.log
+#echo "" > /usr/local/squirrel-ai/logs/service.log
+#echo "" > /usr/local/squirrel-ai/logs/motion.log
+#echo "" > /usr/local/squirrel-ai/logs/detection.log
+#echo "" > /usr/local/squirrel-ai/logs/notification.log
 sudo modprobe v4l2loopback devices=2
-#export FLASK_APP=/usr/local/squirrel-ai/service/web.py
-#sudo -u pi /usr/bin/python3  /usr/local/squirrel-ai/service/web.py
 sudo -u pi /usr/bin/python3  /usr/local/squirrel-ai/service/motionDetection.py
-#sudo -u pi /usr/bin/python3  /usr/local/squirrel-ai/service/videoDetection.py
