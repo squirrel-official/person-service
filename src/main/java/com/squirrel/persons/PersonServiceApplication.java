f366af029cbfffca1f3948802aedf5609933e52a
anil <anil.kumar.ait09@gmail.com>
2023-02-20 00:06:31 +1100

    Upgraded jar

--- src/main/java/com/squirrel/persons/PersonServiceApplication.java
+++ src/main/java/com/squirrel/persons/PersonServiceApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableAspectJAutoProxy
+@EnableWebFlux
 public class PersonServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(PersonServiceApplication.class, args);
