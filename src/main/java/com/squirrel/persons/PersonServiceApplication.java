5eae630d0648bcfb9dfa5ff7a71b2f0f09b16bb2
anil <anil.kumar.ait09@gmail.com>
2024-04-21 14:42:09 +1000

    Webflux added

--- src/main/java/com/squirrel/persons/PersonServiceApplication.java
+++ src/main/java/com/squirrel/persons/PersonServiceApplication.java
@@ -1,6 +1,8 @@
 package com.squirrel.persons;
 
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -11,6 +13,7 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 @EnableScheduling
 @EnableAspectJAutoProxy
 @EnableWebFlux
+@OpenAPIDefinition(info = @Info(title = "APIs", version = "1.0", description = "Documentation APIs v1.0"))
 public class PersonServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(PersonServiceApplication.class, args);
